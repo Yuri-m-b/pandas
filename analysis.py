e4922f882d0d985c3e0d3f4c5bbf51942cf48728
yuri barboza <yuri.barboza@partner.bmw.com.br>
2023-06-28 17:21:13 -0300

    Adding info

--- analysis.py
+++ analysis.py
@@ -1,6 +1,31 @@
+""" analysis """
+
 import pandas as pd
 
-poke = pd.read_csv('pokemon_data.csv')
+poke = pd.read_csv('/home/yuri/Pandas_Tutorial/pandas-master/pokemon_data.csv')
 
-print(poke.tail(5))
+# print(poke)
 
+# # Read Header
+# poke.columns
+
+# # Read each Column
+# print(poke[['Name', 'Type 1', 'HP']])
+
+# # Reach each row
+# print(poke.head(4))
+# print(poke.iloc[1:4])
+
+# # Show only the index and row Name
+# for index, row in poke.iterrows():
+#     print(index, row['Name'])
+
+# poke['Total'] = poke['HP'] + poke['Attack'] + poke['Defense'] + poke['Sp. Atk'] + poke['Sp. Def'] + poke['Speed']
+# print(poke.head(5))
+
+# SortingDescribing Data
+# print(poke.sort_values(['Type 1', 'HP']))
+
+# Making Changes to the data
+poke['Total'] = poke.iloc[:, 4:10].sum(axis=1)
+print(poke.head(5))
